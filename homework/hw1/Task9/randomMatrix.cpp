9620c19385cba5fbd3a407951fe27476450e88bc
J.M <moore.jeremiah.mark@gmail.com>
2019-01-12 10:36:14 -0700

    added to .gitignore and started Task9

---
+++ homework/hw1/Task9/randomMatrix.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <omp.h>
+
+
+//Task: Write a routine that will generate a random matrix of a given size. That is, input the number of rows and columns and output the matrix created by setting each entry in the matrix to a random value between zero and one. 
